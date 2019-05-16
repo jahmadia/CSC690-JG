ccf92542f8471f3f4aff83b98a245a14a0f95001
jahmadia <35792010+jahmadia@users.noreply.github.com>
2019-05-16 16:29:31 -0700

    Add files via upload

---
+++ Beetle/Beetle/GameElements.swift
@@ -0,0 +1,31 @@
+
+import SpriteKit
+
+struct CollisionBitMask {
+    static let birdCategory:UInt32 = 0x1 << 0
+    static let pillarCategory:UInt32 = 0x1 << 1
+    static let flowerCategory:UInt32 = 0x1 << 2
+    static let groundCategory:UInt32 = 0x1 << 3
+}
+
+extension GameScene{
+    func createBird() -> SKSpriteNode {
+        let bird = SKSpriteNode(texture: SKTextureAtlas(named:"player").textureNamed("bird-01"))
+        bird.size = CGSize(width: 50, height: 50)
+        bird.position = CGPoint(x:self.frame.midX, y:self.frame.midY)
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.width / 2)
+        bird.physicsBody?.linearDamping = 1.1
+        bird.physicsBody?.restitution = 0
+        bird.physicsBody?.categoryBitMask = CollisionBitMask.birdCategory
+        bird.physicsBody?.collisionBitMask = CollisionBitMask.pillarCategory | CollisionBitMask.groundCategory
+        bird.physicsBody?.contactTestBitMask = CollisionBitMask.pillarCategory | CollisionBitMask.flowerCategory | CollisionBitMask.groundCategory
+        bird.physicsBody?.affectedByGravity = false
+        bird.physicsBody?.isDynamic = true
+        
+        return bird
+    }
+    
+   
+
+}
